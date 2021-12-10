06ca2b13213465f366e5dafd4c113d9bce004f15
sreeganga <78845970+sreeganga@users.noreply.github.com>
2021-12-10 11:20:27 +0530

    Add files via upload

---
+++ pandas/pandas2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 09:09:33 2021
+
+@author: Administrator
+"""
+
+import pandas as pd
+series1= pd.Series(range(5))
+print(series1)
---
+++ pandas/pandas6.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 09:52:12 2021
+
+@author: Administrator
+"""
+
+import pandas as pd
+list1=[[10,20,30,40,50],[1,2,3],['a','b','c','d','e']]
+df1=pd.DataFrame(list1)
+print(df1)
+l2=['a',1,2.5]
+df2=pd.DataFrame(l2)
+print(df2)
